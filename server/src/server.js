c46862f8267a4415bb74ad163c371f8ae1b9540e
Maxwell200073 <91772625+Maxwell200073@users.noreply.github.com>
2022-02-13 20:19:32 -0600

    finish planets route and model

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 
 const app = require("./app");
+const { loadPlanetData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 5000, () => {
-    console.log(`Listening on port ${process.env.PORT || 5000}`);
-});
+async function startServer() {
+    await loadPlanetData();
+
+    server.listen(process.env.PORT || 5000, () => {
+        console.log(`Listening on port ${process.env.PORT || 5000}`);
+    });
+}
+startServer();
