0768c1ebe83fdb23155dc6e2a3424f40cbcd9efb
Maxwell200073 <91772625+Maxwell200073@users.noreply.github.com>
2022-02-21 17:05:15 -0600

    finish launch testing

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../../app");
 
 describe("Test GET /launches", () => {
-    test("it should get 200 response", async () => {
+    test("should get 200 response", async () => {
         const response = await request(app)
             .get("/launches")
             .expect("Content-Type", /json/)
@@ -11,9 +11,58 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launch", () => {
-    test("it should get 201 response", () => {});
+    const completeLaunchData = {
+        launchDate: "January 1, 2030",
+        target: "Kepler-69",
+        rocket: "Explorer IS1",
+        mission: "Goin Downtown",
+    };
+    const launchDataWithoutDate = {
+        target: "Kepler-69",
+        rocket: "Explorer IS1",
+        mission: "Goin Downtown",
+    };
 
-    test("should fail if improper date", () => {});
+    test("should get a 201 response", async () => {
+        const response = await request(app)
+            .post("/launches")
+            .send(completeLaunchData)
+            .expect("Content-Type", /json/)
+            .expect(201);
 
-    test("should fail if missing fields", () => {});
+        const sentLaunchDate = new Date(
+            completeLaunchData.launchDate
+        ).valueOf();
+        const receivedLaunchDate = new Date(response.body.launchDate).valueOf();
+
+        expect(sentLaunchDate).toBe(receivedLaunchDate);
+
+        expect(response.body).toMatchObject(launchDataWithoutDate);
+    });
+
+    test("should fail if improper date", async () => {
+        // completeLaunchData is assigned an improper date for test
+        completeLaunchData.launchDate = "December 44, 2029";
+        const response = await request(app)
+            .post("/launches")
+            .send(completeLaunchData)
+            .expect("Content-Type", /json/)
+            .expect(400);
+
+        expect(response.body).toStrictEqual({
+            error: "Invalid launch date",
+        });
+    });
+
+    test("should fail if missing fields", async () => {
+        const response = await request(app)
+            .post("/launches")
+            .send(launchDataWithoutDate)
+            .expect("Content-Type", /json/)
+            .expect(400);
+
+        expect(response.body).toStrictEqual({
+            error: "Missing required mission field",
+        });
+    });
 });
